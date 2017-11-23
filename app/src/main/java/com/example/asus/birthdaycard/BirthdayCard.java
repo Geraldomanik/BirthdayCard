eff44ee0d38df8958807f5fd1898acfecf14f06c
Geraldomanik <geraldo.manik.31@gmail.com>
2017-11-23 11:21:27 +0700

    versi 1.0

---
+++ app/src/main/java/com/example/asus/birthdaycard/BirthdayCard.java
@@ -0,0 +1,13 @@
+package com.example.asus.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BirthdayCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_birthday_card);
+    }
+}
